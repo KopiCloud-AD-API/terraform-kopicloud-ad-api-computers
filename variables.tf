f122a8db40c5b434f519a1f1dcf51fc0fef59510
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2023-02-12 15:45:38 +0100

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,13 @@
+##############################################
+## Kopicloud Terraform Provider - Variables ##
+##############################################
+
+variable "kopicloud_token" {
+  type        = string
+  description = "KopiCloud AD API Token"
+}
+
+variable "kopicloud_host" {
+  type        = string
+  description = "KopiCloud AD API Host"
+}
